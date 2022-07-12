ac55de992d66fec673c18ca2609b3362d32708f5
Cote17 <102267647+Cote17@users.noreply.github.com>
2022-07-11 21:52:18 -0400

    Delete Trabajo-Programaci-n-Web-masterv2/proyectosdjango/VeggyVeggy directory

--- Trabajo-Programaci-n-Web-masterv2/proyectosdjango/VeggyVeggy/productos/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import TipoCategoria,Productos
-# Register your models here.
-admin.site.register(TipoCategoria)
-admin.site.register(Productos)
--- Trabajo-Programaci-n-Web-masterv2/proyectosdjango/VeggyVeggy/rest_veggy/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestVeggyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rest_veggy'
-
